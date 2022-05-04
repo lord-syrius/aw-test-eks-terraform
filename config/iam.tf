50656224ab58da16cebfbb326cc6e6f9419549f4
Nico Singh <nico.singh@thoughtworks.com>
2022-05-04 18:14:22 -0400

    rename trigger name

--- config/iam.tf
+++ config/iam.tf
@@ -36,7 +36,7 @@ locals {
 resource "time_sleep" "wait" {
   create_duration = "180s"
   triggers = {
-    cluster_name = data.aws_eks_cluster.cluster.endpoint
+    cluster_endpoint = data.aws_eks_cluster.cluster.endpoint
   }
 }
 resource "kubernetes_config_map_v1_data" "aws_auth_users" {
