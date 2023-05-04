fe60543890ec0624214e83cc13162553d1bc9170
Nico Singh <nicolas.singh@gmail.com>
2023-05-04 11:17:48 -0400

    use exec in kubernetes plugin to get token

--- config/eks.tf
+++ config/eks.tf
@@ -32,8 +32,13 @@ data "aws_eks_cluster_auth" "cluster" {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    command     = "aws"
+  }
 }
+
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
